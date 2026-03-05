5781f566a1ccdf16af35ba20ad0b6e42f58069cd
Matt Warren <mattwar@microsoft.com>
2026-03-04 16:00:40 -0800

    Add some utility tests

---
+++ src/KustoLsp.Tests/Utilities/DeclarationFinderTests.cs
@@ -0,0 +1,353 @@
+﻿using System.Collections.Immutable;
+
+using Kusto.Language;
+using Kusto.Language.Editor;
+using Kusto.Language.Symbols;
+using Kusto.Language.Syntax;
+using Kusto.Lsp;
+
+namespace KustoLspTests;
+
+using static TestHelpers;
+
+[TestClass]
+public class DeclarationFinderTests
+{
+    #region GetSourceLocations Tests
+
+    [TestMethod]
+    public void TestGetSourceLocations_SimpleVariable()
+    {
+        TestGetSourceLocations(
+            """
+            let x = 10;
+            $x
+            """,
+            GlobalState.Default,
+            [new DeclarationLocation { Range = new TextRange(4, 1) }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_Table()   
+    {
+        var table = new TableSymbol("MyTable", "(x: long, y: string)");
+        var globals = GlobalState.Default
+            .WithDatabase(new DatabaseSymbol("db", table));
+
+        TestGetSourceLocations(
+            """
+            MyTable | where $x > 10
+            """,
+            globals,
+            [new DeclarationLocation { Entity = table }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_ColumnInFunction()
+    {
+        var table = new TableSymbol("MyTable", "(x: long, y: string)");
+        var function = new FunctionSymbol("MyFunction", "()", "{ MyTable | extend z=x+5 }");
+
+        var globals = GlobalState.Default.WithDatabase(
+            new DatabaseSymbol("db", 
+                table,
+                function
+                ));
+
+        TestGetSourceLocations(
+            """
+            MyFunction | where $z > 10
+            """,
+            globals,
+            [new DeclarationLocation { Range = new TextRange(19, 1), Entity = function }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_ColumnFromTableInFunction()
+    {
+        // Tests that a column from a table referenced inside a function traces back to the table
+        var table = new TableSymbol("MyTable", "(x: long, y: string)");
+        var function = new FunctionSymbol("MyFunction", "()", "{ MyTable }");
+
+        var globals = GlobalState.Default.WithDatabase(
+            new DatabaseSymbol("db",
+                table,
+                function
+                ));
+
+        TestGetSourceLocations(
+            """
+            MyFunction | where $x > 10
+            """,
+            globals,
+            [new DeclarationLocation { Entity = table }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_NestedFunctionColumn()
+    {
+        // Tests column declared in a function that calls another function
+        var table = new TableSymbol("MyTable", "(x: long, y: string)");
+        var innerFunction = new FunctionSymbol("InnerFunc", "()", "{ MyTable | extend z=x+1 }");
+        var outerFunction = new FunctionSymbol("OuterFunc", "()", "{ InnerFunc | extend w=z+1 }");
+
+        var globals = GlobalState.Default.WithDatabase(
+            new DatabaseSymbol("db",
+                table,
+                innerFunction,
+                outerFunction
+                ));
+
+        TestGetSourceLocations(
+            """
+            OuterFunc | where $w > 10
+            """,
+            globals,
+            [new DeclarationLocation { Range = new TextRange(21, 1), Entity = outerFunction }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_FunctionParameter()
+    {
+        // Tests finding the declaration of a function parameter
+        var function = new FunctionSymbol("MyFunction", "(val: long)", "{ print result=val }");
+
+        var globals = GlobalState.Default.WithDatabase(
+            new DatabaseSymbol("db", function));
+
+        TestGetSourceLocations(
+            """
+            MyFunction(10) | project $result
+            """,
+            globals,
+            [new DeclarationLocation { Range = new TextRange(8, 6), Entity = function }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_MultipleLetStatements()
+    {
+        // Tests that the correct variable is found when multiple let statements exist
+        TestGetSourceLocations(
+            """
+            let a = 1;
+            let b = 2;
+            let c = 3;
+            print $b
+            """,
+            GlobalState.Default,
+            [new DeclarationLocation { Range = new TextRange(16, 1) }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_VariableReferencingVariable()
+    {
+        // Tests a variable that references another variable
+        TestGetSourceLocations(
+            """
+            let x = 10;
+            let y = x + 5;
+            print $y
+            """,
+            GlobalState.Default,
+            [new DeclarationLocation { Range = new TextRange(17, 1) }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_ProjectedColumn()
+    {
+        // Tests a column created via project
+        var table = new TableSymbol("MyTable", "(x: long, y: string)");
+        var globals = GlobalState.Default
+            .WithDatabase(new DatabaseSymbol("db", table));
+
+        TestGetSourceLocations(
+            """
+            MyTable | project newcol = x | where $newcol > 10
+            """,
+            globals,
+            [new DeclarationLocation { Range = new TextRange(18, 6) }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_ExtendedColumn()
+    {
+        // Tests a column created via extend
+        var table = new TableSymbol("MyTable", "(x: long, y: string)");
+        var globals = GlobalState.Default
+            .WithDatabase(new DatabaseSymbol("db", table));
+
+        TestGetSourceLocations(
+            """
+            MyTable | extend computed = x * 2 | where $computed > 10
+            """,
+            globals,
+            [new DeclarationLocation { Range = new TextRange(17, 8) }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_SummarizeColumn()
+    {
+        // Tests a column created via summarize
+        var table = new TableSymbol("MyTable", "(x: long, y: string)");
+        var globals = GlobalState.Default
+            .WithDatabase(new DatabaseSymbol("db", table));
+
+        TestGetSourceLocations(
+            """
+            MyTable | summarize total = sum(x) | where $total > 10
+            """,
+            globals,
+            [new DeclarationLocation { Range = new TextRange(20, 5) }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_DatabaseTable()
+    {
+        // Tests finding a database table reference
+        var table = new TableSymbol("MyTable", "(x: long, y: string)");
+        var globals = GlobalState.Default
+            .WithDatabase(new DatabaseSymbol("db", table));
+
+        TestGetSourceLocations(
+            """
+            $MyTable | take 10
+            """,
+            globals,
+            [new DeclarationLocation { Entity = table }]
+            );
+    }
+
+    [TestMethod]
+    public void TestGetSourceLocations_DatabaseFunction()
+    {
+        // Tests finding a database function reference
+        var function = new FunctionSymbol("MyFunction", "()", "{ print 1 }");
+        var globals = GlobalState.Default
+            .WithDatabase(new DatabaseSymbol("db", function));
+
+        TestGetSourceLocations(
+            """
+            $MyFunction
+            """,
+            globals,
+            [new DeclarationLocation { Entity = function }]
+            );
+    }
+
+    private void TestGetSourceLocations(string queryWithMarker, GlobalState globals, ImmutableList<DeclarationLocation> expected)
+    {
+        var (query, position) = TestHelpers.ExtractPosition(queryWithMarker);
+        var code = KustoCode.ParseAndAnalyze(query, globals);
+        var symbol = GetSymbolAt(code, position);
+        Assert.IsNotNull(symbol, "Expected to find a symbol at the marker position.");
+        var actual = DeclarationFinder.GetSourceDeclarations(globals, code.Syntax, symbol);
+        Assert.HasCount(expected.Count, actual);
+        for (int i = 0; i < expected.Count; i++)
+        {
+            AssertAreEqual(expected[i].Range, actual[i].Range);
+            Assert.AreEqual(expected[i].Entity?.Name, actual[i].Entity?.Name, "location entity");
+        }
+    }
+
+    #endregion
+
+    #region TryGetLocalDeclaration Tests
+
+    [TestMethod]
+    public void TestTryGetLocalDeclaration_VariableFound()
+    {
+        var code = KustoCode.ParseAndAnalyze("let x = 10; x", GlobalState.Default);
+        var symbol = GetReferencedSymbol(code, "x");
+        Assert.IsNotNull(symbol);
+
+        var found = DeclarationFinder.TryGetLocalDeclaration(code.Syntax, symbol, out var declaration);
+
+        Assert.IsTrue(found);
+        Assert.IsNotNull(declaration);
+        Assert.AreEqual("x", ((NameDeclaration)declaration).SimpleName);
+    }
+
+    [TestMethod]
+    public void TestTryGetLocalDeclaration_NotFound()
+    {
+        var table = new TableSymbol("MyTable", "(x: long)");
+        var globals = GlobalState.Default.WithDatabase(new DatabaseSymbol("db", table));
+        var code = KustoCode.ParseAndAnalyze("MyTable", globals);
+
+        // Table symbol won't have a local declaration
+        var found = DeclarationFinder.TryGetLocalDeclaration(code.Syntax, table, out var declaration);
+
+        Assert.IsFalse(found);
+        Assert.IsNull(declaration);
+    }
+
+    #endregion
+
+    #region GetLocalDeclarations Tests
+
+    [TestMethod]
+    public void TestGetLocalDeclarations_SingleDeclaration()
+    {
+        var declarations = DeclarationFinder.GetLocalDeclarations(
+            "let x = 10; x",
+            GlobalState.Default,
+            "x");
+
+        Assert.HasCount(1, declarations);
+    }
+
+    [TestMethod]
+    public void TestGetLocalDeclarations_MultipleDeclarations()
+    {
+        // Multiple queries with same variable name
+        var declarations = DeclarationFinder.GetLocalDeclarations(
+            """
+            let x = 10;
+            x;
+            let x = 20;
+            x
+            """,
+            GlobalState.Default,
+            "x");
+
+        Assert.HasCount(2, declarations);
+    }
+
+    [TestMethod]
+    public void TestGetLocalDeclarations_NoDeclarations()
+    {
+        var declarations = DeclarationFinder.GetLocalDeclarations(
+            "print 1",
+            GlobalState.Default,
+            "nonexistent");
+
+        Assert.HasCount(0, declarations);
+    }
+
+    [TestMethod]
+    public void TestGetLocalDeclarations_ExtendColumn()
+    {
+        var table = new TableSymbol("MyTable", "(x: long)");
+        var globals = GlobalState.Default.WithDatabase(new DatabaseSymbol("db", table));
+
+        var declarations = DeclarationFinder.GetLocalDeclarations(
+            "MyTable | extend computed = x * 2",
+            globals,
+            "computed");
+
+        Assert.HasCount(1, declarations);
+    }
+
+    #endregion
+}
---
+++ src/KustoLsp.Tests/Utilities/ErrorDecoderTests.cs
@@ -0,0 +1,297 @@
+﻿using Kusto.Data.Exceptions;
+using Kusto.Lsp;
+using Kusto.Language;
+using Kusto.Language.Editor;
+
+namespace KustoLspTests;
+
+using static TestHelpers;
+
+[TestClass]
+public class ErrorDecoderTests
+{
+    #region SyntaxException Tests
+
+    [TestMethod]
+    public void TestGetDiagnostic_SyntaxException()
+    {
+        TestGetDiagnostic(
+            "T | were x > y",
+            new SyntaxException("Bad Syntax", null)
+            {
+                ErrorCode = "SYNTAX_ERROR", 
+                Line = 1,
+                CharacterPositionInLine = 5,
+                Token = "were"
+            },
+            new Diagnostic("SYNTAX_ERROR", "Bad Syntax").WithLocation(4, 4)
+            );
+    }
+
+    [TestMethod]
+    public void TestGetDiagnostic_SyntaxException_WithLeadingCommentRemoved()
+    {
+        // Simulates a query where a leading comment was removed before sending to server
+        // Original: "// comment\nT | were x > y"
+        // Sent:     "T | were x > y"
+        var original = "// comment\nT | were x > y";
+        var modified = "T | were x > y";
+        var query = new EditString(original).ReplaceAt(0, "// comment\n".Length, "");
+
+        Assert.AreEqual(modified, query.CurrentText);
+
+        var error = new SyntaxException("Bad Syntax", null)
+        {
+            ErrorCode = "SYNTAX_ERROR",
+            Line = 1,
+            CharacterPositionInLine = 5,
+            Token = "were"
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        // Position should be mapped back to original text (after the comment)
+        Assert.AreEqual("SYNTAX_ERROR", diagnostic.Code);
+        Assert.AreEqual(15, diagnostic.Start); // "// comment\n" (11) + "T | " (4) = 15
+        Assert.AreEqual(4, diagnostic.Length); // "were"
+    }
+
+    [TestMethod]
+    public void TestGetDiagnostic_SyntaxException_MultiLineQuery()
+    {
+        // Error on second line
+        var query = new EditString("T\n| were x > y");
+        var error = new SyntaxException("Bad Syntax", null)
+        {
+            ErrorCode = "SYNTAX_ERROR",
+            Line = 2,
+            CharacterPositionInLine = 3,
+            Token = "were"
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        Assert.AreEqual("SYNTAX_ERROR", diagnostic.Code);
+        Assert.AreEqual(4, diagnostic.Start); // "T\n| " = 4
+        Assert.AreEqual(4, diagnostic.Length);
+    }
+
+    [TestMethod]
+    public void TestGetDiagnostic_SyntaxException_WithMultipleLeadingLinesRemoved()
+    {
+        // Original has multiple comment lines removed
+        var original = "// line 1\n// line 2\nT | were x > y";
+        var query = new EditString(original).ReplaceAt(0, "// line 1\n// line 2\n".Length, "");
+
+        var error = new SyntaxException("Bad Syntax", null)
+        {
+            ErrorCode = "SYNTAX_ERROR",
+            Line = 1,
+            CharacterPositionInLine = 5,
+            Token = "were"
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        // Should map back to position in original text
+        Assert.AreEqual(24, diagnostic.Start); // "// line 1\n// line 2\n" (20) + "T | " (4) = 24
+    }
+
+    [TestMethod]
+    public void TestGetDiagnostic_SyntaxException_NoLineInfo_UsesTokenSearch()
+    {
+        // When line/position info is missing, ErrorDecoder tries to find the token
+        var query = new EditString("T | were x > y");
+        var error = new SyntaxException("Syntax error at token: 'were' [line:position=1:5]", null)
+        {
+            ErrorCode = "SYNTAX_ERROR",
+            Line = -1,
+            CharacterPositionInLine = -1,
+            Token = "were"
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        Assert.AreEqual("SYNTAX_ERROR", diagnostic.Code);
+        // Should find "were" in the query
+        Assert.AreEqual(4, diagnostic.Start);
+    }
+
+    #endregion
+
+    #region Semantic Exception Tests
+
+    [TestMethod]
+    public void TestGetDiagnostic_SemanticException_UnresolvedEntity()
+    {
+        var query = new EditString("UnknownTable | take 10");
+        var error = new RelopSemanticException("Failed to resolve entity 'UnknownTable'", null)
+        {
+            ErrorCode = "SEMANTIC_ERROR"
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        Assert.AreEqual("SEMANTIC_ERROR", diagnostic.Code);
+        // Should find "UnknownTable" in the query
+        Assert.AreEqual(0, diagnostic.Start);
+    }
+
+    [TestMethod]
+    public void TestGetDiagnostic_SemanticException_WithEditedQuery()
+    {
+        // Original: "// find unknown\nUnknownTable | take 10"
+        var original = "// find unknown\nUnknownTable | take 10";
+        var query = new EditString(original).ReplaceAt(0, "// find unknown\n".Length, "");
+
+        var error = new RelopSemanticException("Failed to resolve entity 'UnknownTable'", null)
+        {
+            ErrorCode = "SEMANTIC_ERROR"
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        Assert.AreEqual("SEMANTIC_ERROR", diagnostic.Code);
+        // Position should map to original (after comment)
+        Assert.AreEqual(16, diagnostic.Start);
+    }
+
+    #endregion
+
+    #region KustoBadRequestException Tests
+
+    [TestMethod]
+    public void TestGetDiagnostic_GenericException_SpansWholeQuery()
+    {
+        // Without position info, should span the whole query
+        var query = new EditString("InvalidQuery");
+        var error = new Exception("Bad request");
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        Assert.AreEqual(0, diagnostic.Start);
+        Assert.AreEqual(query.CurrentText.Length, diagnostic.Length);
+    }
+
+    [TestMethod]
+    public void TestGetDiagnostic_GenericException_WithEditedQuery_SpansOriginalRange()
+    {
+        // When there's no position info and query was edited, 
+        // diagnostic should span the original query portion (not the removed parts)
+        var original = "// comment\nInvalidQuery";
+        var query = new EditString(original).ReplaceAt(0, "// comment\n".Length, "");
+
+        var error = new Exception("Bad request");
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        // Should span from the start of the actual query content in original
+        Assert.AreEqual(11, diagnostic.Start); // After "// comment\n"
+        Assert.AreEqual("InvalidQuery".Length, diagnostic.Length);
+    }
+
+    #endregion
+
+    #region EditString Position Mapping Tests
+
+    [TestMethod]
+    public void TestGetDiagnostic_EditString_MiddlePortionRemoved()
+    {
+        // Test case where something in the middle was removed/replaced
+        // Original: "T | /* comment */ where x > 10"
+        // Sent:     "T |  where x > 10"
+        var original = "T | /* comment */ where x > 10";
+        var query = new EditString(original).ReplaceAt(4, "/* comment */".Length, "");
+
+        var error = new SyntaxException("Bad Syntax", null)
+        {
+            ErrorCode = "SYNTAX_ERROR",
+            Line = 1,
+            CharacterPositionInLine = 6, // Position of "where" in modified query
+            Token = "where"
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        // Should map back to "where" in original text
+        Assert.AreEqual(18, diagnostic.Start); // After "T | /* comment */ "
+    }
+
+    [TestMethod]
+    public void TestGetDiagnostic_EditString_ErrorAtEndOfQuery()
+    {
+        // Error at the very end of query
+        var query = new EditString("T | take");
+        var error = new SyntaxException("Unexpected end of query", null)
+        {
+            ErrorCode = "SYNTAX_ERROR",
+            Line = 1,
+            CharacterPositionInLine = 9, // Past end of query
+            Token = null
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        Assert.AreEqual("SYNTAX_ERROR", diagnostic.Code);
+        // Should handle gracefully without throwing
+    }
+
+    [TestMethod]
+    public void TestGetDiagnostic_EditString_MultipleEdits()
+    {
+        // Multiple edits applied to original
+        var original = "// header\nT | where /* filter */ x > 10";
+        var query = new EditString(original)
+            .ReplaceAt(0, "// header\n".Length, "")
+            .ReplaceAt(10, "/* filter */ ".Length, ""); // Position adjusted for first edit
+
+        // After edits: "T | where x > 10"
+        Assert.AreEqual("T | where x > 10", query.CurrentText);
+
+        var error = new SyntaxException("Bad Syntax", null)
+        {
+            ErrorCode = "SYNTAX_ERROR",
+            Line = 1,
+            CharacterPositionInLine = 11, // Position of "x" in modified query
+            Token = "x"
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        // "x" should map back to original position
+        Assert.AreEqual(33, diagnostic.Start); // "// header\nT | where /* filter */ " = 33
+    }
+
+    #endregion
+
+    #region Error Message Parsing Tests
+
+    [TestMethod]
+    public void TestGetDiagnostic_ParsesLinePositionFromMessage()
+    {
+        // Some exceptions embed line:position in the message itself
+        var query = new EditString("T | were x > y");
+        var error = new SyntaxException(
+            "Syntax error: Command could not be parsed: at token: 'were' [line:position=1:5]", 
+            null)
+        {
+            ErrorCode = "SYNTAX_ERROR",
+            Line = -1,
+            CharacterPositionInLine = -1
+        };
+
+        var diagnostic = ErrorDecoder.GetDiagnostic(error, query);
+
+        Assert.AreEqual("SYNTAX_ERROR", diagnostic.Code);
+        Assert.AreEqual(4, diagnostic.Start);
+        Assert.AreEqual(4, diagnostic.Length); // "were"
+    }
+
+    #endregion
+
+    private void TestGetDiagnostic(EditString query, Exception error, Diagnostic expected)
+    {
+        var actual = ErrorDecoder.GetDiagnostic(error, query);
+        AssertAreEqual(expected, actual);
+    }
+}
