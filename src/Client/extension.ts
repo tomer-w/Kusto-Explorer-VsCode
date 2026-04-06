975acd7247061b0e0956537c2d3e2f92935ddbfe
Matt Warren <mattwar@microsoft.com>
2026-04-06 11:17:26 -0700

    A few more tests

--- src/Client/extension.ts
+++ src/Client/extension.ts
@@ -215,7 +215,7 @@ export async function activate(context: ExtensionContext)
     copilot.activate(context, server, connectionManager, resultsViewer);
 
     // Expose internal components for integration tests
-    return { historyManager, connectionManager, resultsViewer };
+    return { historyManager, connectionManager, resultsViewer, server };
 }
 
 export function deactivate(): Thenable<void> | undefined
